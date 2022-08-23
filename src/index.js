d67e5e7640b96f9bb3c4e40c4cebf2b516119736
Kenny Sabangan <ksabangan121@gmail.com>
2022-08-23 15:11:39 -0700

    fix gh-pages

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { searchRobots, requestRobots } from './reducers';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import App from './containers/App';
 import 'tachyons'
 import './index.css'
 
-const logger = createLogger();
+// const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots })
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
                                                             <Provider store={store}>
