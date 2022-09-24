f9f696bb54bb4b7ff25bdff707148b88232860de
Kenny Sabangan <ksabangan121@gmail.com>
2022-09-23 20:04:28 -0700

    downgrade to React 17 for enzyme capatibility

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { shallow, mount, render } from 'enzyme';
 
 const Card = ({ robot }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${robot.id}?200x200`} alt='random robot' />
+      <img src={`https://robohash.org/${robot.id}?size=200x200`} alt='random robot' width='200px' height='200px'/>
       <div>
         <h2>{robot.name}</h2>
         <p>{robot.email}</p>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { searchRobots, requestRobots } from './reducers';
-// import { createLogger } from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import App from './containers/App';
 import 'tachyons'
 import './index.css'
 
-// const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots })
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-                                                            <Provider store={store}>
-                                                              <App />
-                                                            </Provider>);
+let middleware = []
+if (process.env.NODE_ENV === 'production') {
+  middleware = [thunkMiddleware]
+} else {
+  const logger = createLogger();
+  middleware = [thunkMiddleware, logger]
+}
+const store = createStore(rootReducer, applyMiddleware(...middleware))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
